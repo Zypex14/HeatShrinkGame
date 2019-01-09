9857a9f4a8a073f8b3e18bafc4e59f60ce13065d
Zypex <zypex14@gmail.com>
2019-01-09 12:16:03 -0600

    Initial commit

---
+++ src/com/rphegfana/Main.java
@@ -0,0 +1,16 @@
+package com.rphegfana;
+
+public class Main {
+
+//    This is the main method, this is what will happen when the code starts
+    public static void main(String args[]){
+        Main m = new Main();
+        m.start();
+    }
+
+//    This is a function that is what the copy of the blueprint will do
+    public void start(){
+        System.out.println("EZ");
+    }
+
+}
